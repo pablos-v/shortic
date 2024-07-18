f4f58a4b21651333175b5a02174132c9cfebd2f0
Pablos <pablos@list.ru>
2024-07-19 00:16:21 +0300

    app structure created

---
+++ backend-counting-service/src/main/java/org/pablos/backendcountingservice/BackendCountingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.pablos.backendcountingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCountingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCountingServiceApplication.class, args);
+    }
+
+}
---
+++ backend-counting-service/src/test/java/org/pablos/backendcountingservice/BackendCountingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pablos.backendcountingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCountingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
