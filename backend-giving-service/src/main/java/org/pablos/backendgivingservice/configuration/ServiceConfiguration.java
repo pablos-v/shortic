26037d3a242331ef402b4f9ad8cdaecedfe2912e
Pablos <x382@ya.ru>
2025-01-25 18:52:04 +0300

    ready MVP for docker-compose installation, fully functioning from containers

--- backend-giving-service/src/main/java/org/pablos/backendgivingservice/configuration/ServiceConfiguration.java
+++ backend-giving-service/src/main/java/org/pablos/backendgivingservice/configuration/ServiceConfiguration.java
@@ -2,8 +2,14 @@ package org.pablos.backendgivingservice.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class ServiceConfiguration {
@@ -12,4 +18,20 @@ public class ServiceConfiguration {
     public Logger getLogger() {
         return LoggerFactory.getLogger(getClass());
     }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("fullLink",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)));
+    }
+
 }
--- backend-giving-service/src/main/java/org/pablos/backendgivingservice/service/FastLinkService.java
+++ backend-giving-service/src/main/java/org/pablos/backendgivingservice/service/FastLinkService.java
@@ -7,7 +7,9 @@ import org.pablos.backendgivingservice.repository.FastLinkRepository;
 import org.pablos.common.dto.FastLinkDTO;
 import org.pablos.common.exception.*;
 import org.pablos.common.util.CommonUtil;
+import org.slf4j.Logger;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +28,8 @@ public class FastLinkService implements IFastLinkService {
     @Transactional(readOnly = true)
     @Cacheable(
             cacheNames = "fullLink",
-            key = "#shortLink"
+            key = "#shortLink",
+            unless = "#result == null"
     )
     public String getFullLink(final String shortLink) {
         return getFastLink(shortLink).getFullLink();
